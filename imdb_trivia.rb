1ad806b96e20757d3d49cfa7fdf917d72666fc63
Gabe Pressman <pressman4747@gmail.com>
2015-01-23 16:17:09 -0500

    second iteration finished

--- imdb_trivia.rb
+++ imdb_trivia.rb
@@ -2,31 +2,28 @@ require 'imdb'
 require 'awesome_print'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-
-
-
+require 'pry'
 
 get '/' do 
   erb :imdb_home
 end
 
-
-
-
-
-
-
 get '/trivia' do
    erb :trivia_submit
-  
-
 end
 
-
-post '/results' do 
-  
-  searched=Imdb::Search.new(params[:trivia_search])
-  @movie_sample=searched.movies.sample(9)
+post '/results' do
+  searched = Imdb::Search.new(params[:trivia_search])
+  @movies=searched.movies.sample(9)
+  #binding.pry
   erb :results
-  
 end
+
+#post '/results' do
+#  searched = Imdb::Search.new(params[:trivia_search])
+#  movie_sample=searched.movies.sample(9)
+#  @posters=movie_sample.map  do  |movie|
+#    movie.poster
+#  end
+#  erb :results
+#end
